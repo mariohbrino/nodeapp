0125c5e9031e1f14fcef1c4a59241171af7e60c6
Mario Brino <mariobrino@gmail.com>
2026-05-05 23:28:04 -0600

    chore: update comments for clarity in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
-// This will print "Hello, World!" to the console when the script is run.
 import express, { type Request, type Response } from "express";
 
 const app = express();
 const port = 3000;
 
+// Define a default route that display "Hello, World!" when accessed
 app.get("/", (_request: Request, response: Response) => {
   response.send("Hello, World!");
 });
 
+// Start the express server and listen on port 3000
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
