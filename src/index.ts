4dabb52e0381ece749718935b53ccbc6902b782e
Mario Brino <mariobrino@gmail.com>
2026-05-05 23:14:04 -0600

    feat: implement express server with a basic route

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,13 @@
 // This will print "Hello, World!" to the console when the script is run.
-console.log("Hello, World!");
+import express, { type Request, type Response } from "express";
+
+const app = express();
+const port = 3000;
+
+app.get("/", (_request: Request, response: Response) => {
+  response.send("Hello, World!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
